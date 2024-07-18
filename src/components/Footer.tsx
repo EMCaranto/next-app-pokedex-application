21554c94a70e2543c408dc7d3f75dcabee2fb2da
Erickson Caranto <erickson.caranto.m@gmail.com>
2024-07-18 11:26:01 +0800

    refactor: extract Footer component
    Extract the footer into Footer component.
    
    Create new file src/components/Footer.tsx.
    
    Update import in src/app/page.tsx.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <div className="flex h-[48px] w-full border-t-[2px] border-yellow-300 bg-gradient-to-t from-slate-100 to-slate-200"></div>
+  );
+};
