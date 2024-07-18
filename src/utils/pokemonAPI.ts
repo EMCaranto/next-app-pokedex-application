12aead2ae51beac694ed4135326d8bc384cd5182
Erickson Caranto <erickson.caranto.m@gmail.com>
2024-07-18 11:34:38 +0800

    refactor: update pokemonAPI.ts to use updated interfaces and separate fetch logic
    Refactor pokemonAPI.ts to use updated interfaces.
    
    Separate fetch logic into fetchPokemonList and fetchPokemonData functions.
    
    Combine results in getPokemon function.

--- src/utils/pokemonAPI.ts
+++ src/utils/pokemonAPI.ts
@@ -1,68 +1,99 @@
-import { Pokemon } from '@/types/pokemon';
+interface PokeAPIProps {
+  // This are inside the "pokemon.data.results"
+
+  name: string;
+  url: string;
+}
 
 interface PokemonProps {
+  // This are inside the "pokemon.data.results.url"
+
+  id: number;
+  name: string;
+  sprites: string;
+  types: string[];
+}
+
+interface PokeAPIResponseProps {
+  // This are inside the "pokemon.data"
+
+  pokemon: (PokeAPIProps & PokemonProps)[];
+  totalCount: number;
+}
+
+interface PokemonListProps {
   pageParams: number;
   pageSize: number;
 }
 
-interface PokemonResponse {
-  pokemon: Pokemon[];
-  totalCount: number;
-}
+// Fetch Pokémon list from the API
+const fetchPokemonList = async (
+  limit: number,
+  offset: number
+): Promise<{ results: PokeAPIProps[]; count: number }> => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  );
+
+  if (!response.ok) {
+    console.log('An error occured in the fetchPokemonList');
+  }
+
+  return response.json();
+};
+
+const fetchPokemonData = async (pokemonUrl: string): Promise<PokemonProps> => {
+  const response = await fetch(pokemonUrl);
+
+  if (!response.ok) {
+    console.log('An error occured in the fetchPokemonData');
+  }
+
+  const pokemonData = await response.json();
+
+  return {
+    id: pokemonData.id,
+    name: pokemonData.name,
+    sprites: pokemonData.sprites.front_default,
+    types: pokemonData.types.map(
+      (poke_types: { type: { name: string } }) => poke_types.type.name
+    ),
+  };
+};
 
 export const getPokemon = async ({
   pageParams,
   pageSize,
-}: PokemonProps): Promise<PokemonResponse> => {
+}: PokemonListProps): Promise<PokeAPIResponseProps> => {
   const limit = pageSize;
 
-  const offset = limit * pageParams;
+  const offset = limit * pageSize;
   const maxOffset = 1025;
 
-  let pokemon: Pokemon[] = [];
-
   if (offset >= maxOffset) {
-    return { pokemon, totalCount: maxOffset };
+    return { pokemon: [], totalCount: maxOffset };
   }
 
   const dynamicLimit = Math.min(limit, maxOffset - offset);
 
-  const response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon?limit=${dynamicLimit}&offset=${offset}`
-  );
+  const pokemonListData = await fetchPokemonList(dynamicLimit, offset);
+  const totalCount = pokemonListData.count;
 
-  if (!response.ok) {
-    throw new Error('An error occured on getPokemon');
-  }
-
-  const data = await response.json();
-  const totalCount = data.count;
-
-  const promises = data.results.map(
-    async (pokemon: { name: string; url: string }) => {
-      // this will fetch the pokemon url
-      const pokemonUrl = await fetch(pokemon.url);
-
-      // this will fetch the pokemon details
-      const pokemonData = await pokemonUrl.json();
+  const promises = pokemonListData.results.map(
+    async (pokemon: PokeAPIProps) => {
+      const pokemonData = await fetchPokemonData(pokemon.url);
 
       return {
-        name: pokemon.name,
+        // Pokémon List data
         url: pokemon.url,
 
-        // pokemon url data
-        id: pokemonData.id,
-        sprites: pokemonData.sprites.front_default,
-        types: pokemonData.types.map(
-          (type: { type: { name: string } }) => type.type.name
-        ),
+        // Pokémon Url data
+        ...pokemonData,
       };
     }
   );
 
-  const batchPokemon = await Promise.all(promises);
-
-  pokemon = pokemon.concat(batchPokemon);
+  const pokemon = await Promise.all(promises);
 
   return { pokemon, totalCount };
 };
