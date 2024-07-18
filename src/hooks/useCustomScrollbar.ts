8231c5cd0f5a3734e6164d3c03c8a78c7df714f9
Erickson Caranto <erickson.caranto.m@gmail.com>
2024-07-18 11:30:00 +0800

    refactor: extract custom scrollbar logic into useCustomScrollbar hook
    Extract custom scrollbar logic into useCustomScrollbar hook.
    
    Create new file src/hooks/useCustomScrollbar.ts.
    
    Update usage in src/app/page.tsx.

---
+++ src/hooks/useCustomScrollbar.ts
@@ -0,0 +1,72 @@
+import React, { useEffect, useRef } from 'react';
+
+export const useCustomScrollbar = () => {
+  const parentRef = useRef<HTMLDivElement>(null);
+  const childRef = useRef<HTMLDivElement>(null);
+  const thumbRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const parent = parentRef.current;
+    const child = childRef.current;
+    const thumb = thumbRef.current;
+
+    if (!parent || !child || !thumb) return;
+
+    const syncScroll = () => {
+      const parentWidth = 0.75; // 75% of the parent width
+
+      const trackWidth = parent.clientWidth * parentWidth;
+      const thumbWidth = thumb.clientWidth;
+      const maxThumbWidth = trackWidth - thumbWidth;
+
+      const scrollPercentage =
+        child.scrollLeft / (child.scrollWidth - child.clientWidth);
+
+      thumb.style.left = `${scrollPercentage * maxThumbWidth}px`;
+    };
+
+    const syncScrollbar = (event: MouseEvent) => {
+      const parentWidth = 0.75; // 75% of the parent width
+
+      const trackWidth = parent.clientWidth * parentWidth;
+      const thumbWidth = thumb.clientWidth;
+      const maxThumbWidth = trackWidth - thumbWidth;
+
+      const parentClientWidth = 0.125; // 12.5% padding on each side
+
+      const mouse_x =
+        event.clientX -
+        parent.getBoundingClientRect().left -
+        parent.clientWidth * parentClientWidth;
+
+      const scrollPercentage = mouse_x / trackWidth;
+
+      child.scrollLeft =
+        scrollPercentage * (child.scrollWidth * child.clientWidth);
+
+      thumb.style.left = `${Math.min(Math.max(0, mouse_x), maxThumbWidth)}px`;
+    };
+
+    const onMouseMove = (mouseMove: MouseEvent) => {
+      syncScrollbar(mouseMove);
+    };
+
+    const onMouseUp = () => {
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    thumb.addEventListener('mousedown', (event: MouseEvent) => {
+      event.preventDefault();
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    });
+
+    child.addEventListener('scroll', syncScroll);
+
+    syncScroll();
+  }, []);
+
+  return { parentRef, childRef, thumbRef };
+};
